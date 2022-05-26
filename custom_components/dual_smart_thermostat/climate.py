d763397c1e86cdc18d639ba01a2d90fbcca09d88
mark-lawrenson <106143170+mark-lawrenson@users.noreply.github.com>
2022-05-26 19:25:10 +1000

    Stuff it I didn't really need Fan Only anyway!

--- custom_components/dual_smart_thermostat/climate.py
+++ custom_components/dual_smart_thermostat/climate.py
@@ -222,7 +222,6 @@ class DualSmartThermostat(ClimateEntity, RestoreEntity):
         if self.heater_entity_id and self.cooler_entity_id:
             self._hvac_list = [
                 HVACMode.OFF,
-                HVACMode.FAN_ONLY,
                 HVACMode.HEAT_COOL,
             ]
         elif self.ac_mode:
