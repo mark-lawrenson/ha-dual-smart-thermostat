e226a715409894fbfe4fd89e7e8beb7ade83afc7
mark-lawrenson <106143170+mark-lawrenson@users.noreply.github.com>
2022-05-26 19:04:11 +1000

    Remote Heat and Cool modes

--- custom_components/dual_smart_thermostat/climate.py
+++ custom_components/dual_smart_thermostat/climate.py
@@ -222,8 +222,6 @@ class DualSmartThermostat(ClimateEntity, RestoreEntity):
         if self.heater_entity_id and self.cooler_entity_id:
             self._hvac_list = [
                 HVACMode.OFF,
-                HVACMode.HEAT,
-                HVACMode.COOL,
                 HVACMode.HEAT_COOL,
             ]
         elif self.ac_mode:
